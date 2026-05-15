665308c0e064bb99da910b617b9601729b5b8d8a
warxone378 <hondotalent02@gmail.com>
2026-05-15 15:05:32 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-12345')
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///agrozim.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY', '21a19f388c785be5a6e02fbf77f130e5')
+PROVINCES = ['Bulawayo','Harare','Manicaland','Mashonaland Central','Mashonaland East',
+             'Mashonaland West','Masvingo','Matabeleland North','Matabeleland South','Midlands']
+SOIL_TYPES = ['Sandy','Clay','Loam','Silty','Peaty']
+SEED_TYPES = ['Hybrid Maize','Open Pollinated Maize','Wheat','Soybean','Sorghum']
+import os
+BASE_DIR = '/tmp'  # writable directory on Render
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "agrozim.db")}'
